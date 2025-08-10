515912a369d4724a0a461f605e50c255c50b0b46
Brendon Jiang <brendon.jiang1234567890@gmail.com>
2025-08-09 23:53:58 -0400

    set up simple teleop driving

--- all_seaing_lab/setup.py
+++ all_seaing_lab/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'all_seaing_lab'
 
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), 
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml')) + 
+         glob(os.path.join('config', '*.rviz'))
+         ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +28,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'sim = all_seaing_lab.sim:main'
+            'sim_engine = all_seaing_lab.sim_engine:main',
+            'teleop_controller = all_seaing_lab.teleop_controller:main'
         ],
     },
 )
